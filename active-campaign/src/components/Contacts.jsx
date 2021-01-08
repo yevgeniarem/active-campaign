5b0255fa682d140dc240a720dd9bd85d3cc4197f
Yevgenia Rem <yevgeniarem@gmail.com>
2021-01-07 18:02:13 -0800

    use axios instead of fetch

--- active-campaign/src/components/Contacts.jsx
+++ active-campaign/src/components/Contacts.jsx
@@ -1,16 +1,41 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import axios from "axios";
 
 export default function Contacts() {
   const [contacts, setContacts] = useState(); // fill in initial state
 
-  useEffect(() => {
-    async function getContacts() {
-      const contacts = await axios.get("https://sahmed93846.api-us1.com");
-      setContacts(contacts);
-    }
-    getContacts();
-  }, []);
+  // fetch(
+  //   "https://cors-anywhere.herokuapp.com/sahmed93846.api-us1.com/api/3/contacts?status=-1&orders%5Bemail%5D=ASC",
+  //   {
+  //     method: "GET",
+  //     headers: {
+  //       "Api-Token":
+  //         "bcd062dedabcd0f1ac8a568cdcf58660c44d7e79b91763cc1a5d0c03d52c522d851fceb0",
+  //     },
+  //   }
+  // )
+  //   .then((response) => {
+  //     console.log(response);
+  //     setContacts(response);
+  //   })
+  //   .catch((err) => {
+  //     console.error(err);
+  //   });
+
+  axios
+    .get(
+      "https://cors-anywhere.herokuapp.com/sahmed93846.api-us1.com/api/3/contacts?status=-1&orders%5Bemail%5D=ASC",
+      {
+        "Api-Token":
+          "bcd062dedabcd0f1ac8a568cdcf58660c44d7e79b91763cc1a5d0c03d52c522d851fceb0",
+      }
+    )
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
 
   return (
     <>
