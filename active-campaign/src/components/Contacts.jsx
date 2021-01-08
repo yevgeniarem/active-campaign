0358d067dc6339286b63d50ad8009d9633e2ea30
Yevgenia Rem <yevgeniarem@gmail.com>
2021-01-07 18:23:36 -0800

    putting api request into useEffect hook

--- active-campaign/src/components/Contacts.jsx
+++ active-campaign/src/components/Contacts.jsx
@@ -1,46 +1,54 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 export default function Contacts() {
   const [contacts, setContacts] = useState(); // fill in initial state
 
-  // fetch(
-  //   "https://cors-anywhere.herokuapp.com/sahmed93846.api-us1.com/api/3/contacts?status=-1&orders%5Bemail%5D=ASC",
-  //   {
-  //     method: "GET",
-  //     headers: {
-  //       "Api-Token":
-  //         "bcd062dedabcd0f1ac8a568cdcf58660c44d7e79b91763cc1a5d0c03d52c522d851fceb0",
-  //     },
-  //   }
-  // )
-  //   .then((response) => {
-  //     console.log(response);
-  //     setContacts(response);
-  //   })
-  //   .catch((err) => {
-  //     console.error(err);
-  //   });
-
-  axios
-    .get(
-      "https://cors-anywhere.herokuapp.com/sahmed93846.api-us1.com/api/3/contacts?status=-1&orders%5Bemail%5D=ASC",
-      {
-        "Api-Token":
-          "bcd062dedabcd0f1ac8a568cdcf58660c44d7e79b91763cc1a5d0c03d52c522d851fceb0",
+  useEffect(() => {
+    const getContacts = async () => {
+      try {
+        const response = await axios.get(
+          "https://cors-anywhere.herokuapp.com/sahmed93846.api-us1.com/api/3/contacts?status=-1&orders%5Bemail%5D=ASC:443",
+          {
+            headers: {
+              "Api-Token":
+                "bcd062dedabcd0f1ac8a568cdcf58660c44d7e79b91763cc1a5d0c03d52c522d851fceb0",
+            },
+          }
+        );
+        await setContacts(response);
+      } catch (err) {
+        console.error(err);
       }
-    )
-    .then(function (response) {
-      console.log(response);
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
+    };
+    getContacts();
+  }, []);
+
+  console.log(contacts);
+
+  //     const contacts = await axios
+  //       .get(
+  //         "https://cors-anywhere.herokuapp.com/sahmed93846.api-us1.com/api/3/contacts?status=-1&orders%5Bemail%5D=ASC:443",
+  //         {
+  //           headers: {
+  //             "Api-Token":
+  //               "bcd062dedabcd0f1ac8a568cdcf58660c44d7e79b91763cc1a5d0c03d52c522d851fceb0",
+  //           },
+  //         }
+  //       )
+  //       .then(function (response) {
+  //         setContacts(response.data.contacts);
+  //       })
+  //       .catch(function (error) {
+  //         console.log(error);
+  //       });
+  //   };
+  // });
 
   return (
     <>
       <h1>Contacts</h1>
-      <div>{contacts}</div>
+      <div></div>
     </>
   );
 }
