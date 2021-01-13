fb7350eac183c322009b8d4cdaa2193f166dcf61
Yevgenia Rem <yevgeniarem@gmail.com>
2021-01-12 20:48:25 -0800

    deleted unnecessary comments

--- active-campaign/src/components/Contacts.jsx
+++ active-campaign/src/components/Contacts.jsx
@@ -33,26 +33,6 @@ export default function Contacts() {
     setContactData(data);
   }, []);
 
-  // useEffect(() => {
-  //   const getOtherData = async () => {
-  //     try {
-  //       const response = await axios.get(
-  //         "https://cors-anywhere.herokuapp.com/sahmed93846.api-us1.com/api/3/geoIps/2/geoAddress",
-  //         {
-  //           headers: {
-  //             "Api-Token":
-  //               "bcd062dedabcd0f1ac8a568cdcf58660c44d7e79b91763cc1a5d0c03d52c522d851fceb0",
-  //           },
-  //         }
-  //       );
-  //       await console.log("OTHER_DATA: ", response.data);
-  //     } catch (err) {
-  //       console.error(err);
-  //     }
-  //   };
-  //   getOtherData();
-  // }, []);
-
   console.log("CONTACT DATA", contactData);
 
   const contactTagsObj = convertArrayToObject(contactData.contactTags, "id");
@@ -118,29 +98,6 @@ export default function Contacts() {
           );
         })}
       </table>
-      {/* {contactData.contacts.map(
-        ({ id, firstName, lastName, deals, contactTags }) => {
-          // console.log(dealsObj);
-          return (
-            <div key={id}>
-              <div>
-                {firstName} {lastName}
-              </div>
-              <div>Contact Tags:</div>
-              {contactTags.map((ct) => {
-                return <div>{tagsObj[contactTagsObj[ct].tag].tag}</div>;
-              })}
-              <div>Deals:</div>
-              <div>{deals.length}</div>
-              <div>Total Value:</div>
-              {deals.map((d) => {
-                return <div>{dealsObj[d].value}</div>;
-              })}
-              <br />
-            </div>
-          );
-        }
-      )} */}
     </>
   );
 }
