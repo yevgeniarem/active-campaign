37a52ab9600adc6df8786f2a148e9c665592a0af
Yevgenia Rem <yevgeniarem@gmail.com>
2021-01-15 17:05:30 -0800

    hid api key

--- active-campaign/src/utils/constants.js
+++ active-campaign/src/utils/constants.js
@@ -1,7 +1,7 @@
 /* eslint-disable import/no-anonymous-default-export */
+
 export default {
   API_HOST: "sahmed93846.api-us1.com",
   CORS_ANYWHERE: "cors-anywhere.herokuapp.com",
-  API_TOKEN:
-    "bcd062dedabcd0f1ac8a568cdcf58660c44d7e79b91763cc1a5d0c03d52c522d851fceb0",
+  API_TOKEN: process.env.REACT_APP_API_KEY,
 };
